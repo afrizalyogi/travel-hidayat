bbddd4f4153b9bf416a6b7eee89071c9da054db7
Afrizal Yogi <xxgdev@gmail.com>
2022-11-28 08:38:15 +0700

    Add Success Page

---
+++ success.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Sukses</title>
+    <link
+      rel="stylesheet"
+      href="libraries/bootstrap/css/bootstrap.css"
+    />
+    <link rel="stylesheet" href="styles/main.css" />
+    <link
+      href="https://fonts.googleapis.com/css?family=Assistant:200,400,700&&display=swap"
+      rel="stylesheet"
+    />
+    <link
+      href="https://fonts.googleapis.com/css?family=Playfair+Display:400,700&display=swap"
+      rel="stylesheet"
+    />
+  </head>
+  <body>
+  <div class="container">
+			<nav class="row navbar navbar-expand-lg navbar-light bg-white">
+				<div class="navbar-nav ml-auto mr-auto mr-sm-auto mr-lg-0 mr-md-auto">
+					<a class="navbar-brand" href="index.html">
+						<b>Travel Hidayat</b>
+					</a>
+				</div>
+				<ul class="navbar-nav mr-auto d-none d-lg-block">
+					<li>
+						<span class="text-muted"
+							>| &nbsp; Nikmati keindahan yang belum pernah anda rasakan</span
+						>
+					</li>
+				</ul>
+			</nav>
+		</div>
+    <main>
+      <div class="section-success d-flex align-items-center">
+        <div class="col text-center">
+          <img src="images/ic_mail.png" alt="" />
+          <h1>Pembayaran Sukses!</h1>
+          <p>
+            Kami telah mengirimkan email petunjuk berikutnya<br />
+            silahkan baca email anda.
+          </p>
+          <a href="index.php" class="btn btn-home-page mt-3 px-5">
+            Beranda
+          </a>
+        </div>
+      </div>
+    </main>
+    <script src="libraries/jquery/jquery-3.4.1.min.js"></script>
+    <script src="libraries/bootstrap/js/bootstrap.js"></script>
+  </body>
+</html>
